41c3c4e660f20a64ffeaf2fc0540bea1c3d0649c
Tom Xue <tomxue026@gmail.com>
2012-04-02 15:14:49 +0800

    to show how to make a simple server: response with "Hellow World!" once

---
+++ http.js
@@ -0,0 +1,10 @@
+﻿var sys = require("util"),
+    http = require("http");
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("Hello World!");
+    response.close();
+}).listen(8080);
+
+sys.puts("Server running at http://localhost:8080/");
